e55ccd5a49958b08b425c5864c62d9884eabbc2a
Joe-noh <goflb.jh@gmail.com>
2016-05-15 18:39:08 +0900

    functions

--- syntax/src/main.rs
+++ syntax/src/main.rs
@@ -12,4 +12,27 @@ fn main() {
         println!("x = {}, y = {}", x, y);
     }
     println!("x = {}, y = {}", x, y);  // y = 2, not 3
+
+    print_number(x);
+
+    print_sum(x, add_one(y));
+
+    let f: fn(i32) -> i32 = add_one;
+    print_number(f(3));
+}
+
+fn print_number(x: i32) {
+    println!("x is: {}", x);
+}
+
+fn print_sum(x: i32, y: i32) {
+    print_number(x + y);
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1 // == return x+1;
+}
+
+fn diverges() -> ! {
+    panic!("Never returns!");
 }
