a4f30e8faad5651c461f90644eba2e0d8015b6eb
Joe-noh <goflb.jh@gmail.com>
2021-08-13 09:32:16 +0900

    search for pattern from a file

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use clap::{App, Arg};
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let args = App::new("grep-lite")
+        .version("0.1.0")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true)
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let input = args.value_of("input").unwrap();
+
+    let re = Regex::new(pattern).unwrap();
+    let file = File::open(input).unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let l = line.unwrap();
+        match re.find(&l) {
+            Some(_) => println!("{}", l),
+            None => {}
+        }
+    }
 }
